002e35ba7285b63762e8557e78d8d2b1320ecd35
yeongwoo <jyy0223@naver.com>
2024-05-14 15:24:12 +0900

    [feat] color 추가

--- lib/common/const/colors.dart
+++ lib/common/const/colors.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class MyColor {
   // Main
-  static const primary = Color(0xFFFF5A36);
+  static const primary = Color(0xFFFF9C55);
   static const secondary = Color(0xFFFF9A85);
-  static const third = Color(0xFFB42001);
+  static const third = Color(0xFFE65527);
 
   // Text Color
   static const text = Color(0xFF1C1C1E);
@@ -15,7 +15,8 @@ class MyColor {
   static const darkGrey = Color(0xFF767676);
   static const middleGrey = Color(0xFFE5E5EA);
   static const lightGrey = Color(0xFFF4F5F9);
-  static const barrier = Color(0x3f1C1C1E);
-  static const empty = Color(0x00ffffff);
-  static const shadow = Color(0x3f000000);
+  static const barrier = Color(0x3F1C1C1E);
+  static const empty = Color(0x00FFFFFF);
+  static const shadow = Color(0x3F000000);
+  static const red = Color(0xFFFA6230);
 }
