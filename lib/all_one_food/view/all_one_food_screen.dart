c75c8e306f2136efe22065e7d8a2464ed9c0d5fa
yeongwoo <jyy0223@naver.com>
2024-05-14 15:06:27 +0900

    [feat] bottomNavigationItem에 적용될 4개의 화면 추가

---
+++ lib/all_one_food/view/all_one_food_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AllOneFoodScreen extends StatelessWidget {
+  static String get routeName => "all_one_food";
+
+  const AllOneFoodScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/home/view/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String get routeName => "home";
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
