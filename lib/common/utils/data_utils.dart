4feb71ea032810eb68d7ea01c5abcdf651a4e430
yeongwoo <jyy0223@naver.com>
2024-05-14 15:11:21 +0900

    [feat] datetime, moneyString, randomNumber, RandomId, list_shuffled

---
+++ lib/common/utils/data_utils.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:intl/intl.dart';
+
+class DataUtils {
+  static String convertDateTimeToDateString({
+    required DateTime datetime,
+  }) {
+    return '${datetime.year}.${datetime.month.toString().padLeft(2, '0')}.${datetime.day.toString().padLeft(2, '0')}';
+  }
+
+  static String convertDateTimeToDateTimeString({
+    required DateTime datetime,
+  }) {
+    return '${datetime.year}.${datetime.month.toString().padLeft(2, '0')}.${datetime.day.toString().padLeft(2, '0')} ${datetime.hour.toString().padLeft(2, '0')}:${datetime.minute.toString().padLeft(2, '0')}';
+  }
+
+  static String convertPriceToMoneyString({
+    required int price,
+  }) {
+    final NumberFormat numberFormat = NumberFormat('###,###,###,###');
+
+    return numberFormat.format(price);
+  }
+
+  static int getRandomNumber({
+    required int dividerNumber,
+  }) {
+    final randomObj = Random();
+    return randomObj.nextInt(dividerNumber);
+  }
+
+  static List<String> getRandomId({
+    required int dividerNumber,
+    required int count,
+  }) {
+    final randomObj = Random();
+
+    final List<String> ids = [];
+
+    for (int i = 0; i < count; i++) {
+      ids.add(randomObj.nextInt(dividerNumber).toString());
+    }
+
+    return ids;
+  }
+
+  static List<T> getRandomShuffledList<T>(List<T> items) {
+    List<T> newList = List.from(items); // 원본 리스트의 복사본 생성
+    newList.shuffle(Random()); // 복사본 리스트를 랜덤으로 섞기
+    return newList; // 섞인 리스트 반환
+  }
+}
