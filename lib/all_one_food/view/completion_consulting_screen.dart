42a281cdb2c07dbbac8088ab475b1499197e7a40
yeongwoo <jyy0223@naver.com>
2024-05-20 07:13:39 +0900

    [feat] 컨설팅 결과화면 레이아웃 구현

--- lib/all_one_food/view/completion_consulting_screen.dart
+++ lib/all_one_food/view/completion_consulting_screen.dart
@@ -1,3 +1,5 @@
+import 'package:all_one_food/common/component/default_button.dart';
+import 'package:all_one_food/common/const/text_styles.dart';
 import 'package:all_one_food/common/layout/default_app_bar.dart';
 import 'package:all_one_food/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +12,68 @@ class CompletionConsultingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      appbar: DefaultAppBar(title: 'dsaf'),
+      appbar: const DefaultAppBar(title: 'dsaf'),
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _renderTitleAndDescription(
+              title: '리포팅',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            _renderTitleAndDescription(
+              title: '리포팅',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            _renderTitleAndDescription(
+              title: '리포팅',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            _renderTitleAndDescription(
+              title: '리포팅',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            _renderTitleAndDescription(
+              title: '리포팅',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40.0),
+              child: PrimaryButton(
+                onPressed: () {},
+                child: const Text('전문가 상담 요청'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _renderTitleAndDescription({
+    required String title,
+    required String description,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Placeholder(),
+          Text(
+            title,
+            style: MyTextStyle.bodyTitleMedium,
+          ),
+          const SizedBox(height: 8.0),
+          Text(
+            description,
+            style: MyTextStyle.descriptionRegular,
+          ),
         ],
       ),
     );
