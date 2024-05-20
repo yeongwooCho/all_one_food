2c5c7661ec4933be49c74447e8b93a5066c6f6d0
yeongwoo <jyy0223@naver.com>
2024-05-20 10:59:07 +0900

    [feat] 상품 상세페이지 추가 및 상품 더미 데이터 변경

--- lib/product/provider/product_provider.dart
+++ lib/product/provider/product_provider.dart
@@ -1,4 +1,3 @@
-import 'package:all_one_food/common/const/image_path.dart';
 import 'package:all_one_food/common/utils/data_utils.dart';
 import 'package:all_one_food/product/model/product_model.dart';
 import 'package:all_one_food/product/provider/category_provider.dart';
@@ -50,15 +49,50 @@ class ProductStateNotifier extends StateNotifier<List<ProductModel>> {
   }
 
   List<ProductModel> getItems() {
+    final productNames = [
+      '치즈 왕관 팝콘',
+      '꿀맛 크림 와퍼 샌드',
+      "컬러풀 데일리 매크롱 세트",
+      "골든 칩 크리스피 딜라이트",
+      "컬러풀 캔디 트로피칼 믹스",
+      "피트니스 고섬유 바",
+      "초코칩 아이스크림 쿠키",
+      "블랙 화이트 크림 샌드위치",
+      "트로피칼 프루티 젤리 웜",
+      "골든 크런치 호두 스낵",
+      "허브 & 칠리 프로틴 스낵",
+      "피칸 초코칩 브레드"
+    ];
+
+    final prices = [
+      4300,
+      2700,
+      3400,
+      1480,
+      2300,
+      5300,
+      3400,
+      2200,
+      1400,
+      2200,
+      3300,
+      2300,
+    ];
+
     return List.generate(
-      12,
+      productNames.length,
       (index) => ProductModel(
         id: index,
-        name: '하이하이$index 이다.}',
-        price: 12345,
+        name: productNames[index],
+        price: prices[index],
         sale: index + 10,
-        mainImageUrl: 'asset/img/product/${index}.png',
-        detailImageUrls: [ImagePath.logo, ImagePath.logo],
+        mainImageUrl: 'asset/img/product/$index.png',
+        detailImageUrls: [
+          'asset/img/product/$index/0.png',
+          'asset/img/product/$index/1.png',
+          'asset/img/product/$index/2.png',
+          'asset/img/product/$index/3.png',
+        ],
         isLike: true,
         categoryIds: [1, 2, 3],
       ),
