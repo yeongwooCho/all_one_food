a6195d892dc9e6daeca6c6341d7192ff4edc47f1
yeongwoo <jyy0223@naver.com>
2024-05-20 10:12:47 +0900

    [feat] 컨셉 결과 화면 구현

--- lib/all_one_food/view/completion_consulting_screen.dart
+++ lib/all_one_food/view/completion_consulting_screen.dart
@@ -1,5 +1,6 @@
 import 'package:all_one_food/all_one_food/view/reserve_consulting.dart';
 import 'package:all_one_food/common/component/default_button.dart';
+import 'package:all_one_food/common/const/colors.dart';
 import 'package:all_one_food/common/const/text_styles.dart';
 import 'package:all_one_food/common/layout/default_app_bar.dart';
 import 'package:all_one_food/common/layout/default_layout.dart';
@@ -14,7 +15,7 @@ class CompletionConsultingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      appbar: const DefaultAppBar(title: 'dsaf'),
+      appbar: const DefaultAppBar(title: '컨셉 결과'),
       child: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: Column(
@@ -26,25 +27,41 @@ class CompletionConsultingScreen extends StatelessWidget {
                   '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
             ),
             _renderTitleAndDescription(
-              title: '리포팅',
+              title: '제품 설명',
               description:
                   '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
             ),
             _renderTitleAndDescription(
-              title: '리포팅',
+              title: '특징 및 장점',
               description:
                   '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
             ),
             _renderTitleAndDescription(
-              title: '리포팅',
+              title: '영양정보',
               description:
                   '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
             ),
             _renderTitleAndDescription(
-              title: '리포팅',
+              title: '제품 종류',
               description:
                   '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
             ),
+            _renderTitleAndDescription(
+              title: '판매 포인트',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            _renderTitleAndDescription(
+              title: '관련 이미지 아이디어',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            _renderTitleAndDescription(
+              title: '희망 생산원가',
+              description:
+                  '동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 동해물과 백두산이 마르고 ',
+            ),
+            _renderRecommandPrice(),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40.0),
               child: PrimaryButton(
@@ -80,6 +97,78 @@ class CompletionConsultingScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _renderRecommandPrice() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Text(
+            '추천 생산원가',
+            style: MyTextStyle.bodyTitleMedium,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Text(
+            '고객님께 추천 드리는 AOF를 통한 우수 컨설팅 사례',
+            style: MyTextStyle.minimumRegular.copyWith(
+              color: MyColor.darkGrey,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 150.0,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                    color: MyColor.middleGrey,
+                  ),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                width: 230.0,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0, vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        '크라운 콩 단백질 에너지바',
+                        style: MyTextStyle.bodyRegular,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        '콩 단백질을 함유하여 비건 고객 맞춤 건강한 에너지바',
+                        style: MyTextStyle.descriptionRegular,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        '1,500원',
+                        style: MyTextStyle.descriptionRegular,
+                        textAlign: TextAlign.end,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(width: 8.0);
+            },
+            itemCount: 4,
+          ),
+        ),
+      ],
     );
   }
 }
